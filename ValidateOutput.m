e9ba23e6f9a7da2b95d1d1d4279669ce0842d289
BarkenBark <oscar.henrik.bark@gmail.com>
2017-09-19 17:10:34 +0200

    Ändrade ordningen på input, samt rensade gammal bortkommenterad kod

--- ValidateOutput.m
+++ ValidateOutput.m
@@ -1,10 +1,6 @@
-function classificationError = ValidateOutput( output, targetOutput )
-% Calculates the accuracy of network  output
-
-  %accuracyVector = [output'.*targetOutput]==1; %gives vector of agreeing elements
-  %accuracy = sum( accuracyVector )/length(output); %fraction of agreeing elements
-
-  %accuracy = length(find(output==targetOutput))/length(output);
+function classificationError = ValidateOutput(targetOutput, output )
+%Returns the classification error between targetOutput and output, given
+%the definition from task PM
   
   p = length(output);
   if length(targetOutput) ~= p
