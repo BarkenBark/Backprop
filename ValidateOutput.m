5c6388b71fcbafab8f1539fd993b972b7293cb3d
BarkenBark <oscar.henrik.bark@gmail.com>
2017-09-19 16:47:10 +0200

    Skrev färdigt denna, ger nu classification error som han ber om i PM

--- ValidateOutput.m
+++ ValidateOutput.m
@@ -1,10 +1,17 @@
-function accuracy = ValidateOutput( output, targetOutput )
+function classificationError = ValidateOutput( output, targetOutput )
 % Calculates the accuracy of network  output
 
   %accuracyVector = [output'.*targetOutput]==1; %gives vector of agreeing elements
   %accuracy = sum( accuracyVector )/length(output); %fraction of agreeing elements
 
-  accuracy = length(find(output==targetOutput))/length(output);
+  %accuracy = length(find(output==targetOutput))/length(output);
+  
+  p = length(output);
+  if length(targetOutput) ~= p
+    error('Number of target patterns must equal number of output patterns');
+  end
+  
+  classificationError = 1/(2*p)*sum(abs(targetOutput-sign(output)));
   
 end
 
