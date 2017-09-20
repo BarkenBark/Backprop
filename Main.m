5f71bfbbbec640f70c8607e840c05b3c01d1194c
BarkenBark <oscar.henrik.bark@gmail.com>
2017-09-20 12:08:03 +0200

    Nu sparas energy och error för varje körning i en cell(nbrOfRuns)

--- Main.m
+++ Main.m
@@ -19,25 +19,27 @@ beta = 1;
 weightInterval = [-.2 .2];
 thresholdInterval = [-1 1];
 
-NUMBER_OF_ITERATIONS = 10^6;
-NUMBER_OF_RUNS = 1;
+NUMBER_OF_ITERATIONS = 10^5;
+NUMBER_OF_RUNS = 2;
 
 %%
+error = cell(NUMBER_OF_RUNS, 1);
+energy = cell(NUMBER_OF_RUNS, 1);
 
 for iRun = 1:NUMBER_OF_RUNS
   fprintf('Commencing run %d.\n', iRun)
   
-  networkDimensions = [2 1];
+  networkDimensions = [2 4 1];
   network = InitializeNetwork(networkDimensions, weightInterval, thresholdInterval); %Returns cell of random weight matrices
 
-  [newNetwork, energy] = AsynchronouslyTrainNetwork(network, trainData, validData, ...
+  [newNetwork, energy{iRun}] = AsynchronouslyTrainNetwork(network, trainData, validData, ...
     beta, eta, NUMBER_OF_ITERATIONS); %Update weight matrices
   output = ForwardPropagate(newNetwork, input, beta)';
-  error = ValidateOutput(targetOutput, output);
+  error{iRun} = ValidateOutput(targetOutput, output);
 
-  fprintf(strcat('Run %d complete. Classification error on', ...
-    ' validation set: %d.\n Press space to continue.\n'), iRun, error);
-  pause
+  fprintf(strcat('Run %d complete. \nClassification error on', ...
+    ' validation set: %d.\nPress space to continue.\n\n'), iRun, error{iRun});
+  
 end
 
 
